70c0cf1a404f2adb1861f3c657daaa660992afb7
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-09-27 01:26:23 -0400

    Implement authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
+const crypto = require('crypto')
 const express = require('express')
+const bodyParser = require('body-parser')
 const mysql = require('promise-mysql')
 const session = require('express-session')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 require('dotenv').config();
+
+const hash = (password, salt) => crypto
+  .pbkdf2Sync(password, salt, 10000, 64, 'sha512')
+  .toString('base64');
 
 (async () => {
   connection = await mysql.createConnection({
@@ -18,20 +24,32 @@ require('dotenv').config();
     done(null, user.id)
   })
 
-  passport.deserializeUser((id, done) => {
-    done(null, /*TODO: get user by id*/{ id: 0 })
+  passport.deserializeUser(async (id, done) => {
+    const rows = await connection.query('SELECT * FROM `users` WHERE `id`=?', [id])
+    if (rows.length == 0)
+      throw new Error(`User ${id} no longer exists!`)
+    done(null, rows[0])
   })
 
-  passport.use('register', new LocalStrategy(
-    (username, password, done) => {
-      console.log(`Created user @${username} with password ${password}`)
-      done(null, { id: 0 })
-    }
-  ))
-
   passport.use('login', new LocalStrategy(
-    (username, password, done) => {
-      console.log(`Logged in as user @${username} with password ${password}`)
+    {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    async (username, password, done) => {
+      const rows = await connection.query('SELECT * FROM `users` WHERE `email`=?', [username])
+      if (rows.length === 0) {
+        done(null, false, { message: 'Incorrect email or password' })
+        return
+      }
+
+      const user = rows[0]
+      if (user.password !== hash(password, user.salt)) {
+        done(null, false, { message: 'Incorrect email or password' })
+        return
+      }
+
+      console.log(`Logged user in`)
       done(null, { id: 0 })
     }
   ))
@@ -39,21 +57,38 @@ require('dotenv').config();
   const PORT = 3000
   const app = express()
 
+  app.use(bodyParser.json())
   app.use(session({
     resave: false, saveUninitialized: true, secret: 'hhehh'
   }))
   app.use(passport.initialize())
   app.use(passport.session())
 
-  app.post('/register', passport.authenticate('register', {
-    successRedirect: '/',
-    failureRedirect: '/register'
-  }))
+  app.post('/register', async (req, res, next) => {
+    const salt = crypto.randomBytes(64).toString('hex')
+    const password = hash(req.body.password, salt)
+    const result = await connection.query(
+      'INSERT INTO `users` (`first_name`, `last_name`, `email`, `phone`, `password`, `salt`, `location`, `is_deliverer`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+      [req.body.first_name, req.body.last_name, req.body.email, req.body.phone, password, salt, req.body.location, req.body.is_deliverer])
+    const id = result.insertId
+
+    console.log(`Registered user ${id}`)
+    passport.authenticate('login', {
+      successRedirect: '/',
+      failureRedirect: '/login'
+    })(req, res, next)
+  })
 
   app.post('/login', passport.authenticate('login', {
     successRedirect: '/',
     failureRedirect: '/login'
   }))
+
+  app.post('/logout', (req, res) => {
+    req.logout()
+    console.log('Logged user out')
+    res.redirect('/')
+  })
 
   app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
